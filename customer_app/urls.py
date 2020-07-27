54e4718a38713b238c8ae5e375fbf6a2c5b06754
Manish B.C <49949864+manish25A@users.noreply.github.com>
2020-07-27 11:32:37 +0545

    managed whole code and made changes on dashboard

--- customer_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import path
-from main_app.views import createviews, renderviews
-
-urlpatterns = [
-    # render urls
-
-]
-urlpatterns += staticfiles_urlpatterns()
