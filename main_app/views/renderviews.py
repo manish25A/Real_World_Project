c255ec8acfe0a34653c87a7c7260598edb918d45
successbhattarai <bhabisbhai@gmail.com>
2020-08-22 01:38:53 +0545

    CRUD on Admin n Customer

--- main_app/views/renderviews.py
+++ main_app/views/renderviews.py
@@ -16,8 +16,35 @@ def dashboard(request):
 def AdminDashboard(request):
     return render(request, 'admindash.html')
 
+def CreateAdmin(request):
+    return render(request, 'createAdmin.html')
+
+def ViewAdmin(request):
+    return render(request, 'adminView.html')
+
+def UpdateAdmin(request):
+    return render(request, 'editAdmin.html')
+
+def DeleteAdmin(request):
+    return render(request, 'editAdmin.html')
+
+
+
 def CustomerDashboard(request):
     return render(request, 'customerdash.html')
+
+
+def CreateCustomer(request):
+    return render(request, 'createCustomer.html')
+
+def ViewCustomer(request):
+    return render(request, 'customerView.html')
+
+def UpdateCustomer(request):
+    return render(request, 'editCustomer.html')
+
+def DeleteCustomer(request):
+    return render(request, 'editCustomer.html')
 
 
 def login(request):
