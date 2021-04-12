e8f1c2911c8dd1f5cdc3f0ad30e53d84cbdc5697
Manish <manysh23@gmail.com>
2021-04-12 21:50:15 +0545

    comit

--- main_app/models/models.py
+++ main_app/models/models.py
@@ -15,7 +15,7 @@ class Job(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=300)
     description = models.TextField()
-    location = models.CharField(max_length=150)
+    location1 = models.CharField(max_length=150)
     type = models.CharField(choices=JOB_TYPE, max_length=10)
     last_date = models.DateTimeField()
     company_name = models.CharField(max_length=100)
